72e2a2f7f974b6d1d85c0f6948de2cb62486ba7a
Ajay Ahire <ajay.mojoestech@gmail.com>
2021-07-26 14:11:02 +0530

    crud

--- Node Projects/Practice1/Angular/Registration/src/app/login/login.component.ts
+++ Node Projects/Practice1/Angular/Registration/src/app/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { MustMatch } from '../mustMatchValidator';
+import { AuthService } from './../auth.service';
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(  private fb : FormBuilder, private AuthService: AuthService , private Router: Router) { }
 
   ngOnInit() {
+
+    this.loginForm = this.fb.group({
+      reg_email: "",
+      reg_password : ""
+    });
   }
 
 }
